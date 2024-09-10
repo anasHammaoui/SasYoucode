9a706bb7a6432d12521a10607e12014cfd9df537
anashm <anashammaoui07@gmail.com>
2024-09-10 15:36:08 +0200

    refixing challenge 12

--- day01/variables/ch12/ch12.c
+++ day01/variables/ch12/ch12.c
@@ -4,6 +4,10 @@ int main() {
     int num;
     printf("Entrer un nombre de quatre chifre: ");
     scanf("%d",&num);
+    if (num < 1000 || num > 9999) {
+        printf("Numiro invalide!\n");
+        return 1;
+    }
     int inverse = (num %10)*1000+ ((num/10)%10)*100 + (((num/10)/10)%10)*10 + ((((num/10)/10)/10)%10);
     printf("%d",inverse);
 }
