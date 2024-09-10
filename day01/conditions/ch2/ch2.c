d69e8527703e277068bc12487616f6bcf8398d0e
anashm <anashammaoui07@gmail.com>
2024-09-10 12:01:36 +0200

    adding challenge 2

---
+++ day01/conditions/ch2/ch2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Entrer un caractere: ");
+    scanf("%c",&ch);
+    switch(ch) {
+        case 'a':
+        printf("Voyelle.\n");
+        break;
+        case 'e':
+        printf("Voyelle.\n");
+        break;
+        case 'i':
+        printf("Voyelle.\n");
+        break;
+        case 'o':
+        printf("Voyelle.\n");
+        break;
+        case 'u':
+        printf("Voyelle.\n");
+        break;
+        case 'y':
+        printf("Voyelle.\n");
+        break;
+        case 'A':
+        printf("Voyelle.\n");
+        break;
+        case 'E':
+        printf("Voyelle.\n");
+        break;
+        case 'I':
+        printf("Voyelle.\n");
+        break;
+        case 'O':
+        printf("Voyelle.\n");
+        break;
+        case 'U':
+        printf("Voyelle.\n");
+        break;
+        case 'Y':
+        printf("Voyelle.\n");
+        break;
+        default:
+        printf("Non voyelle\n");
+    }
+}
