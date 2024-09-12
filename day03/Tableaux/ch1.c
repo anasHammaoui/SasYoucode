97a88a7c43c302df446af71d4430d237850c66e5
anashm <anashammaoui07@gmail.com>
2024-09-12 10:58:21 +0200

    adding day03 challenges

---
+++ day03/Tableaux/ch1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int table[5] = {1,2,3,4,5};
+    printf("%d\n",table[0]);
+    printf("%d\n",table[1]);
+    printf("%d\n",table[2]);
+    printf("%d\n",table[3]);
+    printf("%d\n",table[4]);
+}
