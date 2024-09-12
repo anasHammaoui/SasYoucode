4b70cc41653589e5bd30083bb55d1e162a7c0255
anashm <anashammaoui07@gmail.com>
2024-09-12 13:13:46 +0200

    adding ch13

---
+++ day03/Tableaux/ch13/ch13.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int nbrElem;
+    // ajouter les elements
+    printf("Entrer le nombre d'elements: ");
+    scanf("%d",&nbrElem);
+    int table[nbrElem];
+    for (int i = 0; i < sizeof(table)/sizeof(table[0]);i++) {
+        printf("Entrer l'element %d: ", i+1);
+        scanf("%d",&table[i]);
+    }
+    // verifier et afficher si l'element est pair
+    for (int i = 0; i < sizeof(table)/sizeof(table[0]);i++) {
+        if (table[i] % 2 != 0) {
+         printf("%d ", table[i]);
+        }
+    }
+}
